ae1769d809012bb7a1454e59924746bf7fec2610
natalie-todd <natalie.todd15@gmail.com>
2019-10-12 20:27:35 -0600

    10 by 10 rows visually

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 import Square from './Square';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import { tsConstructorType } from '@babel/types';
 const { useCallback, PureComponent } = React;
+
+
 const App = () => {
 
   const createGrid = useCallback(() => {
-    const grid = Array.from({ length: 10 },(_, i) => 
-    Array.from({ length: 10 },(_, j) => 
-    [i, j].join(','),))
-    return grid.map(row => row.map(xy => <Square key={xy}/>))
+    const grid = Array.from({ length: 10 }, (_, i) =>
+      Array.from({ length: 10 }, (_, j) =>
+        [i, j].join(',')))
+
+    return grid.map(row => row.map(xy => <Square key={xy} />))
   },
     [],
   );
 
   return (
     <div>
-      {createGrid()}
+      <Grid item xs={3} className="Square-Container">
+        {createGrid()}
+      </Grid>
     </div>
   );
 }
--- src/Square.js
+++ src/Square.js
@@ -3,21 +3,11 @@ import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function Square() {
-    return (
-        <div>
-              <ButtonGroup size="small" aria-label="small outlined button group">
-                <Button>O</Button>
-                {/* <Button>T</Button>
-                <Button>T</Button>
-                <Button>O</Button>
-                <Button>T</Button>
-                <Button>T</Button>
-                <Button>O</Button>
-                <Button>T</Button>
-                <Button>T</Button>
-                <Button>T</Button> */}
-              </ButtonGroup>
-              </div>
-    );
-  }
-  
+  return (
+    <div>
+      <ButtonGroup size="small" aria-label="small outlined button group">
+        <Button>O</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
